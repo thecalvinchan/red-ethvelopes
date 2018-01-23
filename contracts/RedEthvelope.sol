e2639365e82a6a9c3207ff5d8723be45957a19de
Calvin Chan <calvin.chan.h@gmail.com>
2018-01-22 19:45:46 -0500

    Updated contract to reference costInWei and save metadataUrl slice instead of string

--- contracts/RedEthvelope.sol
+++ contracts/RedEthvelope.sol
@@ -13,27 +13,29 @@ contract ERC721Meta {
 contract RedEthvelope is ERC721Token, ERC721Meta, Ownable {
   using strings for *;
 
-  string private metadataHostUrl;
+  // Cost for creating an ethvelope (taken by the contract)
   uint256 private costInWei;
-
-  function RedEthvelope(string _url, uint256 _costInWei) public {
-    metadataHostUrl = _url;
-    costInWei = _costInWei;
-  }
+  // Keeps track of ETH balance owner is allowed to withdraw
+  uint256 private ownerBalance;
+  // Keeps track of ETH balance in each ethvelope
+  mapping(uint256 => uint256) tokenIdToWei;
+  // Metadata Host URL for retrieving information about ethvelope
+  slice private metadataHostUrl;
+  // We don't really care about anything except the 8 bits representing an Ethvelope
+  uint8[] public redEthvelopes;
 
   function changeCostInWei(uint256 _wei) public onlyOwner {
     costInWei = _wei;
   }
 
   function changeMetadataUrl(string _url) public onlyOwner {
-    metadataHostUrl = _url;
+    metadataHostUrl = _url.toSlice();
   }
 
-  // Keeps track of ETH balance in each ethvelope
-  mapping(uint256 => uint256) tokenIdToWei;
-
-  // Keeps track of ETH balance owner is allowed to withdraw
-  uint256 private ownerBalance;
+  function RedEthvelope(string _url, uint256 _costInWei) public {
+    changeMetadataUrl(_url);
+    changeCostInWei(_costInWei);
+  }
 
   // Withdraws wei as owner
   function withdrawAsOwner(uint256 _wei) public onlyOwner {
@@ -42,17 +44,13 @@ contract RedEthvelope is ERC721Token, ERC721Meta, Ownable {
     msg.sender.transfer(_wei);
   }
 
-  // Initially used an array of Structs but
-  // We don't really care about anything except the 8 bits representing an Ethvelope
-  uint8[] public redEthvelopes;
-
   // Generates a random number from 0 to 256 based on the last block hash
   // https://gist.github.com/alexvandesande/259b4ffb581493ec0a1c
   function randomGen(uint256 seed) constant returns (uint8 randomNumber) {
     return (uint8(sha3(block.blockhash(block.number-1), seed ))%256);
   } 
 
-  function _createEthvelope(uint8 _dna, address _owner) internal returns (uint256 ethvelopeId) {
+  function _createEthvelope(uint8 _dna, address _owner) private returns (uint256 ethvelopeId) {
     uint256 memory id = redEthvelopes.push(_dna) - 1;
     mint(_owner, id);
     return id;
@@ -70,17 +68,19 @@ contract RedEthvelope is ERC721Token, ERC721Meta, Ownable {
   }
 
   function tokenMetadata(uint256 _tokenId) public view returns (string infoUrl) {
-    return metadataHostUrl.toSlice().concat(bytes32(redEthvelopes[_tokenId]));
+    return metadataHostUrl.concat(bytes32(redEthvelopes[_tokenId]));
   }
 
   function createAndSendEthvelope(address _to) public {
+    // Sender must pay at least costInWei to send Ethvelope
     require(msg.value >= costInWei);
+    // Cannot send Ethvelope to yourself
     require(msg.sender != _to);
-    ownerBalance += 1 finney;
+    ownerBalance += costInWei;
     uint256 memory id = _createEthvelope(randomGen(msg.value), _to);
-    deposit(id, msg.value - 1 finney);
+    // Deducts costInWei before depositing remaining balance into Ethvelope
+    deposit(id, msg.value - costInWei);
     return tokenMetadata(id);
   }
-
 
 }
